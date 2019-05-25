ed034658a5fea1c7778b11b08588f7dd1b3dc438
Yamilka Vásquez <yamiivasquezv@gmail.com>
2019-05-25 15:04:26 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+/*conexion a la base de datos
+function connect(){
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rl2013',
+    database: 'VGgatewayDB',
+    port: 3306,
+    insecureAuth : true
+  });
+}*/
+
